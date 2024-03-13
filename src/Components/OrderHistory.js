1de8f1b2dc8c789494004de6d98243f40f59e9fe
sharmasmit <smeet.sharma007@gmail.com>
2024-03-13 19:20:52 +0530

    Some Changes Added

--- src/Components/OrderHistory.js
+++ src/Components/OrderHistory.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Table from "./Table";
+import { Link } from "react-router-dom";
 
 function OrderHistory() {
   return (
@@ -10,65 +11,81 @@ function OrderHistory() {
         <Table />
         <div className="orderHistory-inner">
           <div className="orderHistory-box">
-            <h4 className="topH1">
-              SAL-001202301170001 <span className="order-prise">$141.05</span>
-            </h4>
+            <Link to="/HistoryDetails">
+              <h4 className="topH1">
+                SAL-001202301170001 <span className="order-prise">$141.05</span>
+              </h4>
+            </Link>
             <h5 className="dateDetails">20 Mar 2023, 6.00 pm</h5>
             <h5 className="order-review">Pending</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$19.80</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">20 Mar 2023, 5.50 pm</h5>
             <h5 className="order-review">Received</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$50.20</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">20 Mar 2023, 6.00 pm</h5>
             <h5 className="order-review">Completed</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$25.80</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">20 Feb 2023, 4.00 pm</h5>
             <h5 className="order-review">Completed</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$10.20</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">15 Feb 2023, 11.20 am</h5>
             <h5 className="order-review canceled">Canceled</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$50.20</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">15 Feb 2023, 11.00 am</h5>
             <h5 className="order-review">Completed</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$9.20</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">10 Feb 2023, 11.45 am</h5>
             <h5 className="order-review canceled">Canceled</h5>
           </div>
 
           <div className="orderHistory-box">
+          <Link to="/HistoryDetails">
             <h4 className="topH1">
               SAL-001202301170001 <span className="order-prise">$30.20</span>
             </h4>
+            </Link>
             <h5 className="dateDetails">01 Feb 2023, 12.00 pm</h5>
             <h5 className="order-review">Completed</h5>
           </div>
@@ -88,7 +105,9 @@ function OrderHistory() {
               10
             </button>
 
-            <button className="nextBtn"><img src={require("../Images/RightBtnArrow.png")} /></button>
+            <button className="nextBtn">
+              <img src={require("../Images/RightBtnArrow.png")} />
+            </button>
           </div>
         </div>
       </div>
